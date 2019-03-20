35284c8a1a688eae59260f1cb5edb9ae8eadb86d
rt-brown <47324919+rt-brown@users.noreply.github.com>
2019-03-20 07:30:27 -0700

    showPage function creation

--- js/script.js
+++ js/script.js
@@ -2,44 +2,62 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
+/***
+   Add your global variables that store the DOM elements you will
+   need to reference and/or manipulate.
+
+   But be mindful of which variables should be global and which
+   should be locally scoped to one of the two main functions you're
+   going to create. A good general rule of thumb is if the variable
+   will only be used inside of a function, then it can be locally
    scoped to that function.
 ***/
 
+//identify parent of <li>
+const studentList = document.querySelector(".student-list");
+//identify children of studentList. this should equal all of the student records
+const studentRecords = studentList.children;
+const buttonsPerpage = 10;
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
+
+/***
+   Create the `showPage` function to hide all of the items in the
    list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
+   Pro Tips:
+     - Keep in mind that with a list of 54 students, the last page
        will only display four.
      - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+     - Remember that a function `parameter` goes in the parens when
+       you initially define the function, and it acts as a variable
+       or a placeholder to represent the actual function `argument`
+       that will be passed into the parens later when you call or
+       "invoke" the function
 ***/
 
+const showPage = (list, page) => {
+ const firstIndex = (page * buttonsPerpage) - buttonsPerpage;
+ const lastIndex = (page * buttonsPerpage) - 1;
+ for (let i = 0; i < studentRecords.length; i += 1){
+   if (i >= firstIndex && i <= lastIndex){
+     console.log(studentRecords[i]);
+     studentRecords[i].style.visibility = 'visible';
+   }
+ }
+
+};
+
+showPage(studentRecords, 1);
 
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+/***
+   Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
