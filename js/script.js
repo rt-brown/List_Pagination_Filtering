64b9a1e43c657f55d0838633705f0f030eb508c4
rt-brown <47324919+rt-brown@users.noreply.github.com>
2019-03-28 06:18:07 -0700

    Finished event listeners and highlighted buttons

--- js/script.js
+++ js/script.js
@@ -1,47 +1,7 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
-//identify parent of <li>
-
 const studentList = document.querySelector(".student-list");
-//identify children of studentList. this should equal all of the student records
 const studentRecords = studentList.children;
 const itemsPerpage = 10;
 
-
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
 
 const showPage = (list, page) => {
  const firstIndex = (page * itemsPerpage) - itemsPerpage;
@@ -56,13 +16,6 @@ const showPage = (list, page) => {
 
 };
 showPage(studentList, 1);
-//showPage(studentRecords, 6);
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
 
 const appendPageLinks = (list) => {
   const numberOfpages = Math.ceil(studentList.childElementCount/itemsPerpage);
@@ -82,30 +35,20 @@ const appendPageLinks = (list) => {
     links.appendChild(buttonLi);
   }
 
-  links.addEventListener('click', (e) => {
-    let page = e.target.textContent;
-    showPage(studentList, page);
-  } );
-
   const aTags = document.getElementsByTagName('A')
 
   for (let i = 0; i < aTags.length; i++){
-    aTags[i].className = 'inactive';
+    aTags[i].addEventListener('click', (event) => {
+      let page = event.target.textContent;
+      showPage(studentList, page);
+      for (let i = 0; i < aTags.length; i++){
+        aTags[i].className = 'inactive';
       }
-
-  document.addEventListener('click', (event) =>{
-    if(event.target.tagName === 'A'){
-      event.target.className = 'active';
-    }
-  })
-
-
+      event.target.className = 'active'
+    })
+  }
 };
 
 
 
 appendPageLinks(studentList);
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
